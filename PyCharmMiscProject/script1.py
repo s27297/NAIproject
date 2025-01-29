fada92516619e317275ce28b34cba7eff0360ad4
s27297 <123184307+s27297@users.noreply.github.com>
2025-01-29 15:22:37 +0000

    Update script1.py

--- PyCharmMiscProject/script1.py
+++ PyCharmMiscProject/script1.py
@@ -1,5 +1,5 @@
 from tdIf import classiffy_sentence
 
-text="This comment seems offensive and should be banned"
+text=input("Enter a sentence: ")
 classification = classiffy_sentence(text)
 print("Classification:", classification)
